9df4af213fca94da6b6c457a39ddfd98ce454d90
Nikola Smilyanski <96414322+nsmilyanski@users.noreply.github.com>
2022-08-17 18:18:16 +0300

    Add createVendor updateVendor and patchVendor.

--- src/main/java/guru/springframework/spring5webfluxrest/controllers/VendorController.java
+++ src/main/java/guru/springframework/spring5webfluxrest/controllers/VendorController.java
@@ -1,11 +1,11 @@
 package guru.springframework.spring5webfluxrest.controllers;
 
+import guru.springframework.spring5webfluxrest.domain.Category;
 import guru.springframework.spring5webfluxrest.domain.Vendor;
 import guru.springframework.spring5webfluxrest.repositories.VendorRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.reactivestreams.Publisher;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -27,5 +27,33 @@ public class VendorController {
     @GetMapping("/{id}")
     public Mono<Vendor> getVendorById(@PathVariable String id) {
         return vendorRepository.findById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    Mono<Void> create(@RequestBody Publisher<Vendor> vendorStream) {
+        return vendorRepository.saveAll(vendorStream).then();
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    Mono<Vendor> update(@PathVariable String id, @RequestBody Vendor vendor) {
+        vendor.setId(id);
+        return vendorRepository.save(vendor);
+    }
+
+    @PatchMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    Mono<Vendor> patchCategory(@PathVariable String id, @RequestBody Vendor vendor) {
+        Vendor foundVendor = vendorRepository.findById(id).block();
+
+        if (foundVendor.getFirstName() == null || foundVendor.getLastName() == null ||
+                vendor.getFirstName() != foundVendor.getFirstName()) {
+            foundVendor.setFirstName(vendor.getFirstName());
+            foundVendor.setLastName(vendor.getLastName());
+            return vendorRepository.save(foundVendor);
+        }
+
+        return Mono.just(foundVendor);
     }
 }
